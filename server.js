f6df7e5c1216cb471d6011633454d43a53a00ac1
Jesus Alberto <jesyrom0825@gmail.com>
2021-07-28 22:53:22 -0500

    agregue puerto 80

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 
 
 app.use('/', (req, res)=>{
+
     res.end('<h1>Hola Mundo 2</h1>')
 })
 
-app.listen(3000, ()=>{
+app.listen(80, ()=>{
     console.log('Server corriendo en puerto 3000')
 })
