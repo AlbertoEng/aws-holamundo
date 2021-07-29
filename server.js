ed94e5e74756c18d4d540ab806a8d7336f1d696b
Jesus Alberto <jesyrom0825@gmail.com>
2021-07-28 22:10:15 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.use('/', (req, res)=>{
+    res.end('<h1>Hola Mundo</h1>')
+})
+
+app.listen(3000, ()=>{
+    console.log('Server corriendo en puerto 3000')
+})
